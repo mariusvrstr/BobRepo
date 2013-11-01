701f499f9efda8e7e389e2539159ac75ea5d517f
Riaan Geldenhuys <riaang@3fifteen.co.za>
2013-11-01 22:16:44 +0200

    Winphone project re-added. Dsl contracts copied over.

---
+++ Presentation/WP8/BusinessLayer/Contracts/DSL/Users/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace JBOB.Users
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public IEnumerable<UserRoleEnum> Roles { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("User: {0}, Surename: {2} ({2})", this.Name, this.Surname, this.Login);
+        }
+    }
+}
