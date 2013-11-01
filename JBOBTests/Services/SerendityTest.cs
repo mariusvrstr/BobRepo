f3f9eb0c4a201a028f63a225c866b949265ad99b
Marius <marius.vrstr@gmail.com>
2013-11-01 16:07:42 +0200

    Serendipty
    All Serendipity changes.

---
+++ JBOBTests/Services/SerendityTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JBOB.Serendipty;
+using Services.Interaction;
+using Services.Services;
+
+namespace SerendiptyTestProject
+{
+    [TestClass]
+    public class SerTest
+    {
+        [TestMethod]
+        public void RetriveMetaDataTest()
+        {
+            var service = ServiceFactory.CreateSerendiptyMeta();
+
+            var response = service.RetrieveMetaData("Apples grow on trees while Apple Macs are grown in China");
+            if (string.IsNullOrEmpty(response))
+            {
+                Assert.Fail("No response from serpendipity");
+            }
+        }
+    }
+}
