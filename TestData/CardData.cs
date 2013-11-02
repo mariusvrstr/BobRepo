7f5eb66188a074e17a5764c1fd4e0486ba142f7d
Riaan Geldenhuys <riaang@3fifteen.co.za>
2013-11-02 14:43:46 +0200

    Updated Test Data

--- TestData/CardData.cs
+++ TestData/CardData.cs
@@ -20,7 +20,7 @@ namespace JBOB.TestData
 
                 WalkTheTalk = context.CardRepository.AddCard(new Card { Id = "543325", Name = "702 Walk The Talk", Description = "Do a 5 or 10 kilometer fun run. It's all for charity!", Weight = 50 });
                 Spca = context.CardRepository.AddCard(new Card { Id = "662255", Name = "SPCA Walk a dog", Description = "Walk a dog, just for fun. Do it for an hour and earn the points.", Weight = 20 });
-                Mandela = context.CardRepository.AddCard(new Card { Id = "543321", Name = "67 Min of my time to Cotlands", Description = "o	Cotlands is committed to filling the medico-socio-economic gaps in communities, by providing a range of services that will ensure the provision of food security, reverse the spread of HIV/AIDS, create jobs, and foster self-sustainability.", Weight = 120 });
+                Mandela = context.CardRepository.AddCard(new Card { Id = "543321", Name = "67 Min of my time to Cotlands", Description = "Cotlands is committed to filling the medico-socio-economic gaps in communities, by providing a range of services that will ensure the provision of food security, reverse the spread of HIV/AIDS, create jobs, and foster self-sustainability.", Weight = 120 });
 
                 context.SaveChanges();
             }
